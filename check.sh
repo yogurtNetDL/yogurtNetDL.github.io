73e72b62e7fd7d6137ae83464c4273653ef9abc8
yogurtNetDL <yogurtNetDL@gmail.com>
2017-07-05 21:26:58 -0400

    Update check.sh

--- check.sh
+++ check.sh
@@ -10,7 +10,7 @@ do
 				echo "checking $NAME" > working.txt
 				gcloud compute instances describe $NAME --zone $ZONE | grep status > cur.txt;
 				value=$(<cur.txt);
-				if grep "TERM" cur.txt
+				if ! grep "RUNNING" cur.txt
 				then
 					gcloud compute instances delete $NAME --zone $ZONE -q;
 					gcloud compute --project "master-chariot-92404" instances create "$NAME" --zone "$ZONE" --machine-type "n1-highcpu-2" --subnet "default" --service-account "453970989833-compute@developer.gserviceaccount.com" --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --image "debian-8-jessie-v20170619" --image-project "debian-cloud" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "$NAME" --metadata "startup-script=mkdir /home/hilcj0001/hello;wget http://yogurtnetdl.github.io./namdrun.sh; source namdrun.sh" --preemptible;
